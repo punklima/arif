e52bdba7a43208dea646c39c6dead55b7db88b9e
punklima <bloodyice.iip@gmail.com>
2016-08-04 16:45:41 +0700

    Add files via upload
    testing

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel='icon' type='image/png' href='images/lgegg.png'>
+<title>HALAMAN UTAMA</title>
+<link rel="stylesheet" href="index.css" type="text/css" media="screen" />
+<style type="text/css">
+
+body { 
+     font-size: 0.7em; 
+	 background-image: url(images/bg1a.jpg);
+	 background-repeat: no-repeat;
+
+}
+</style>
+</head>
+
+<body>
+<div id="content_enggang">
+<img src="images/enggang.png" width="250" height="250" border="0">
+</div>
+
+<div id="content_apl">
+<img src="images/apl.png" width="450" height="50" border="0">
+</div>
+
+
+<div id="content_tulis">
+<img src="images/lay.png" width="350" height="30" border="0">
+</div>
+
+<div id="content_versi">
+<img src="images/versi.png" width="100" height="10" border="0">
+</div>
+
+
+<div id="content_login">
+<?
+include ('login.php');
+?>
+
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
